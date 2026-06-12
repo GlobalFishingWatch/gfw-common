82296012380c307ceaa754d154122115a9d808e4
Matias Piano <matias@globalfishingwatch.org>
2026-06-12 10:45:24 -0300

    Adds datetime validation

--- src/gfw/common/cli/validations.py
+++ src/gfw/common/cli/validations.py
@@ -33,3 +33,20 @@ def valid_date(s: str) -> date:
     except ValueError as e:
         msg = "Not a valid date: '{0}'. Expected format is YYYY-MM-DD".format(s)
         raise argparse.ArgumentTypeError(msg) from e
+
+
+def valid_datetime(s: str) -> datetime:
+    """Use with argparse to validate a datetime parameter.
+
+    Example Usage:
+    parser.add_argument(
+        '--datetime',
+        type=valid_datetime,
+        default='2019-01-01T00:00:00'
+    )
+    """
+    try:
+        return datetime.fromisoformat(s)
+    except ValueError as e:
+        msg = "Not a valid datetime: '{0}'. Expected format is YYYY-MM-DDTHH:MM:SS".format(s)
+        raise argparse.ArgumentTypeError(msg) from e
